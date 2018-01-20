76ddb10078b46cbcef9c966337bd0ee95ca34647
wallen <wallen_weel@outlook.com>
2018-01-21 00:04:22 +0800

    Adds test

---
+++ karma.conf.js
@@ -0,0 +1,101 @@
+// Karma configuration
+// Generated on Sat Jan 20 2018 20:45:24 GMT+0800 (中国标准时间)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*.spec.js'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/scripts/Keypad.js': ['coverage'],
+      'test/**/*.spec.js': ['rollup']
+    },
+
+
+    rollupPreprocessor: {
+      output: {
+        name: 'keypad',
+        format: 'umd',
+        sourcemap: 'inline'
+      },
+      plugins: [
+        require('rollup-plugin-node-resolve')(),
+        require('rollup-plugin-postcss')({
+          plugins: [
+            require('postcss-simple-vars')(),
+            require('postcss-nested')(),
+            require('postcss-cssnext')()
+          ],
+          extensions: [ '.css' ]
+        }),
+        require('rollup-plugin-babel')({
+          exclude: 'node_modules/**',
+          plugins: ['external-helpers']
+        })
+      ]
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
---
+++ test/keypad.spec.js
@@ -0,0 +1,22 @@
+import Keypad from '../src/main'
+
+describe('Keypad', () => {
+  const kypd = new Keypad({
+    mobile: false
+  })
+
+  it('should be injected into document.body', () => {
+    expect(!!document.querySelector(kypd.prefix('elem', 'wrap'))).toBe(true);
+  })
+
+  it('should be hide at beginning.', () => {
+    const status = kypd.prefix('attr', 'status')
+
+    expect(kypd.wrap.getAttribute(status)).toBe('ready')
+    
+    for (const [name, keypad] of Object.entries(kypd.keypads)) {
+      expect(keypad.getAttribute(status)).toBe('ready')
+    }
+    kypd.show()
+  })
+})
